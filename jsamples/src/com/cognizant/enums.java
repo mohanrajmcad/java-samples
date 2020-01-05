cd396ede070efa5d7f312f36b9371811334fc0dd
Mohanraj <mohanrajmcad@gmail.com>
2020-01-05 14:28:15 +0000

    sample

---
+++ jsamples/src/com/cognizant/enums.java
@@ -0,0 +1,14 @@
+package com.cognizant;
+
+public class enums {
+	 enum Level {
+		    LOW,
+		    MEDIUM,
+		    HIGH
+		  }
+
+		  public static void main(String[] args) {
+		    Level myVar = Level.MEDIUM; 
+		    System.out.println(myVar);
+		  }
+}
